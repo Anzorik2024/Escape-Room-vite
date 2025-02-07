fdf531350f5077a96233a897f91c2adf2ae9f6c5
ivan <rineroriner@gmail.com>
2025-02-07 17:24:29 +0700

    подключил редьюсер к хранилищу

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,15 @@ import {configureStore} from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
 import { mainReducer } from './main-slice/main-slice';
 import { filterProcessReducer } from './filter-process/filter-process';
+import { userReducer } from './user-process/user-process';
 
 
 export const api = createAPI();
 export const store = configureStore({
   reducer: {
     main: mainReducer,
-    filterProcess: filterProcessReducer
+    filterProcess: filterProcessReducer,
+    userProcess: userReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
