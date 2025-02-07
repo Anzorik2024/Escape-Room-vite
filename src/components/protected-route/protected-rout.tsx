291f6da6f88838048edabf499a28c56c78f6ac4a
ivan <rineroriner@gmail.com>
2025-02-07 09:03:14 +0700

    подправлени протектед роут

--- src/components/protected-route/protected-rout.tsx
+++ src/components/protected-route/protected-rout.tsx
@@ -1,36 +1,36 @@
-// import type { ReactNode } from 'react';
-// import type { Location } from 'react-router-dom';
-// //import { selectUserInfo } from '../../store/selectors';
+import type { ReactNode } from 'react';
+import type { Location } from 'react-router-dom';
+import { AppRoute } from '../../const/app-route';
+//import { selectUserInfo } from '../../store/selectors';
 
-// import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
-// import { AppRoute } from '../../const/app-route';
-// //import { useAppSelector } from '../../hooks/use-app-selector';
+//import { useAppSelector } from '../../hooks/use-app-selector';
 
 
-// type ProtectedRouteProps = {
-//   children: ReactNode;
-//   onlyAuth?: boolean;
-// }
+type ProtectedRouteProps = {
+  children: ReactNode;
+  onlyAuth?: boolean;
+}
 
-// type FromState = {
-//   from?: Location;
-// };
+ type FromState = {
+   from?: Location;
+};
 
-// export default function ProtectedRoute({ children, onlyAuth }: ProtectedRouteProps) {
+export default function ProtectedRoute({ children, onlyAuth }: ProtectedRouteProps) {
 
-//   const location: Location<FromState> = useLocation() as Location<FromState>;
+  const location = useLocation();
 
-//   //const user = useAppSelector(selectUserInfo);
-//   const user = true;
+  //const user = useAppSelector(selectUserInfo);
+  const user = true;
 
-//   if (onlyAuth && user) {
-//     const from = location.state?.from || { pathname: AppRoute.MainPage};
-//     return <Navigate to={from} />;
-//   }
-//   if (!onlyAuth && !user) {
-//     return <Navigate state={{ from: location}} to={AppRoute.LoginPage} />;
-//   }
-//   return children;
-// }
+  if (onlyAuth && user) {
+    const from = (location.state as FromState)?.from || { pathname: AppRoute.MainPage};
+    return <Navigate to={from} />;
+  }
+  if (!onlyAuth && !user) {
+    return <Navigate state={{ from: location}} to={AppRoute.Login} />;
+  }
+  return children;
+}
 
