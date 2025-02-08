fa598901c58ffdf39187fc3d3a63b8f90cf972ed
ivan <rineroriner@gmail.com>
2025-02-09 01:01:52 +0700

    удаление лишних констант

--- src/const.ts
+++
@@ -1,8 +0,0 @@
-import { LevelTypeSample,typeQuestSample, typeQuestMainSample } from './types/quest/quest';
-
-
-
-
-
-
-export { levelType,RequestStatus, typeQuest, typeQuestMain, typeQuestMainImage,valueTypeQuest};
